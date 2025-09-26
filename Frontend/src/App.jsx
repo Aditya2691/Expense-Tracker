c9df35330163a3d4e54ffbf2ba135ec6f05ea10b
Aditya2691 <shinde.aditya2691@gmail.com>
2025-09-26 13:53:36 +0530

    Update frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ToastContainer } from 'react-toastify'
+import { Route, Routes, useLoction } from 'react-router-dom'
+import { useContext, useEffect } from 'react'
+import { AppContext } from './Context/AppContext'
+import SideBar from './Components/SideBar'
+import Dashboard from './Pages/Dashboard'
+import History from './Components/History'
+import ViewTrasaction from './Pages/ViewTrasaction'
+import Income from './Pages/Income'
+import Expenses from './Pages/Expenses'
+import IncomeTransaction from './Pages/IncomeTransaction'
+import ExpensesTransaction from './Pages/ExpensesTransaction'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+
+  const location = useLoction()
+  const { token, fetchIncome, fetchExpense } = useContext(AppContext)
+
+  const hideMainLayout = ["/view-Transaction", "/add-Income", "/add-Expense", "/income-Transaction",  "/expense-Transaction","/login","/register" ].includes(location.pathname)
+
+  useEffect(()=>{
+    if(token){
+      fetchIncome(),
+      fetchExpense()
+    }
+  },[token,location.pathname])
+
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <ToastContainer />
+      <SideBar />
+      {!hideMainLayout ? (
+        <div className='flex flex-row w-full overflow-auto'>
+          <div className='flex-1 w-1/2'>
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+            </Routes>
+          </div>
+          <div className=' flex-2 flex-col md:w-1/3 hidden lg-flex overflow-auto'>
+            <Routes>
+              <Route path='/' element={<History />} />
+            </Routes>
+          </div>
+
+
+        </div>
+      ) : (
+        <div className='flex-1 max-h-screen w-full overflow-auto'>
+          <Routes>
+            <Route path='/view-Transaction' element={<ViewTrasaction/>}/>
+            <Route path='/add-Income' element={<Income/>}/>
+            <Route path='/add-Expense' element={<Expenses/>}/>
+            <Route path='/income-Transaction' element={<IncomeTransaction/>}/>
+            <Route path='/expense-Transaction' element={<ExpensesTransaction/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
+          </Routes>
+          </div>
+      )}
+    </div>
   )
 }
 
